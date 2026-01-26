0c628939ea35cb0d29b43442a0beee481608ddc0
Abi Wildan <muhamad11wildan4n@gmail.com>
2026-01-27 01:21:49 +0700

    feat: create router

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,71 @@
-import { useForm } from "react-hook-form"
-import { SingleInput, Button, RadioInputSmoke, RadioInputGender, Checkbox } from "./components/Input"
+import { useForm } from "react-hook-form";
+import {
+  SingleInput,
+  Button,
+  RadioInputSmoke,
+  RadioInputGender,
+  Checkbox,
+} from "./components/Input";
+import { Link } from "react-router";
 
+function App() {
+  const { register, handleSubmit, reset } = useForm({
+    defaultValues: {
+      fullName: "",
+      age: "",
+      gender: "",
+      smoking: "",
+      cigaretteBrands: [],
+    },
+  });
 
-export default function App() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm()
+  const onSubmit = (data) => {
+    console.log("Form Data:", data);
+  };
 
-
-  const onSubmit = (data) => console.log(data)
-
-
-  // console.log(watch("example")) // watch input value by passing the name of it
-
+  const handleReset = () => {
+    reset();
+  };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-10 mx-auto my-10justify-center max-w-[640px] text-[rgb(32,33,36)] ">
-      <SingleInput title="Full Name" secondTitle="What is your name" placeholder="Nama Anda" />
-      <SingleInput title="Age" secondTitle="Berapa umur anda" placeholder="Umur Anda" />
-      <RadioInputGender header="Apa Jenis Kelamin Anda" text="Pilih salah satu" />
-      <RadioInputSmoke header="Apakah anda merokok?" text="Pilih salah satu" />
-      <Checkbox />
-      {/* <div htmlFor="name" className="flex flex-col gap-5 py-5 px-10 border border-[#dadce0] rounded-md bg-[#fff] ">
-        <div className="title ">
-          <h1 className="text-[24px]">Jika anda pernah merokok, rokok apa yang pernah anda coba </h1>
-          <span className="text-[#1f1f1f]">Pilih beberapa jika anda pernah merokok </span>
-      </div>
-      <label forhtml="gudanggaramfilter">
-        <input type="checkbox" id="gudanggaramfilter" name="gudanggaramfilter" value="gudanggaramfilter" className="mx-3"/>
-        Gudang garam filter
-      </label>
-      </div> */}
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-10 mx-auto my-10 justify-center max-w-[640px] text-[rgb(32,33,36)] ">
+      <SingleInput
+        title="Full Name"
+        secondTitle="What is your name"
+        placeholder="Nama Anda"
+        register={register}
+        name="fullName"
+      />
+      <SingleInput
+        title="Age"
+        secondTitle="Berapa umur anda"
+        placeholder="Umur Anda"
+        register={register}
+        name="age"
+      />
+      <RadioInputGender
+        header="Apa Jenis Kelamin Anda"
+        text="Pilih salah satu"
+        register={register}
+      />
+      <RadioInputSmoke header="Apakah anda merokok?" text="Pilih salah satu" register={register} />
+      <Checkbox register={register} />
       <div className="w-full flex gap-5">
-        <Button text="Submit" styleColor="text-[#fff] bg-[#777777]"/>
-        <Button text="Kosongkan Formulir" styleColor="text-[#777777] bg-[transparent]"/>
+        <Button text="Submit" styleColor="text-[#fff] bg-[#777777]" type="submit" />
+        <Button
+          text="Kosongkan Formulir"
+          styleColor="text-[#777777] bg-[transparent]"
+          onClick={handleReset}
+          type="button"
+        />
       </div>
+      <Link
+        to="/result"
+        className="w-full text-center py-3 my-5 bg-red-400 rounded"
+      >
+        lihat data
+      </Link>
     </form>
-  )
+  );
 }
+export { App };
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,93 +1,117 @@
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 
 function SingleInput(props) {
-      const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors },
-      } = useForm()
-    
-    
-    return (
-    <label htmlFor="name" className="flex flex-col gap-5 py-5 px-10 border border-[#dadce0] rounded-md bg-[#fff] ">
-        <div className="title ">
-          <h1 className="text-[24px]">{props.title}</h1>
-          <span className="text-[#1f1f1f]">{props.secondTitle}</span>
-        </div>
-        <input
-          placeholder={props.placeholder}
-          className="text-[14px] outline-none border-b-1 border-indigo-300 focus:border-indigo-700 focus:border-b-2 focus:ring-0" 
-          defaultValue="test" {...register("example")} />
-      </label>
-    )
+  const { register, name } = props;
+
+  return (
+    <label
+      htmlFor={name}
+      className="flex flex-col gap-5 py-5 px-10 border border-[#dadce0] rounded-md bg-[#fff]"
+    >
+      <div className="title">
+        <h1 className="text-[24px]">{props.title}</h1>
+        <span className="text-[#1f1f1f]">{props.secondTitle}</span>
+      </div>
+
+      <input
+        id={name}
+        placeholder={props.placeholder}
+        className="text-[14px] outline-none border-b border-indigo-300 focus:border-indigo-700 focus:border-b-2 focus:ring-0"
+        {...(register && name ? register(name) : {})}
+      />
+    </label>
+  );
 }
 
 function RadioInputValue(props) {
-    return (
-        <label htmlFor={props.value}>
-          <input type="radio" id={props.value} name={props.value} value={props.value} className="mx-3" />
-          {props.valuetext}
-        </label>
-    )
+  return (
+    <label htmlFor={props.value}>
+      <input
+        type="radio"
+        id={props.value}
+        value={props.value}
+        className="mx-3"
+        {...(props.register && props.name ? props.register(props.name) : {})}
+      />
+      {props.valuetext}
+    </label>
+  );
 }
-
 
 function RadioInputGender(props) {
-    return (
-      <div htmlFor="name" className="flex flex-col gap-5 py-5 px-10 border border-[#dadce0] rounded-md bg-[#fff] ">
-        <div className="title ">
-          <h1 className="text-[24px]">{props.header}</h1>
-          <span className="text-[#1f1f1f]">{props.text}</span>
-        </div>
-        <RadioInputValue value="lakilaki" valuetext="Laki laki" />
-        <RadioInputValue value="perempuan" valuetext="Perempuan" />
+  return (
+    <div
+      htmlFor="name"
+      className="flex flex-col gap-5 py-5 px-10 border border-[#dadce0] rounded-md bg-[#fff] "
+    >
+      <div className="title ">
+        <h1 className="text-[24px]">{props.header}</h1>
+        <span className="text-[#1f1f1f]">{props.text}</span>
       </div>
-    )
+      <RadioInputValue value="lakilaki" valuetext="Laki laki" register={props.register} name="gender" />
+      <RadioInputValue value="perempuan" valuetext="Perempuan" register={props.register} name="gender" />
+    </div>
+  );
 }
 function RadioInputSmoke(props) {
-    return (
-      <div htmlFor="name" className="flex flex-col gap-5 py-5 px-10 border border-[#dadce0] rounded-md bg-[#fff] ">
-        <div className="title ">
-          <h1 className="text-[24px]">{props.header}</h1>
-          <span className="text-[#1f1f1f]">{props.text}</span>
-        </div>
-        <RadioInputValue value="ya" valuetext="Ya" />
-        <RadioInputValue value="tidak" valuetext="Tidak" />
+  return (
+    <div
+      htmlFor="name"
+      className="flex flex-col gap-5 py-5 px-10 border border-[#dadce0] rounded-md bg-[#fff] "
+    >
+      <div className="title ">
+        <h1 className="text-[24px]">{props.header}</h1>
+        <span className="text-[#1f1f1f]">{props.text}</span>
       </div>
-    )
+      <RadioInputValue value="ya" valuetext="Ya" register={props.register} name="smoking" />
+      <RadioInputValue value="tidak" valuetext="Tidak" register={props.register} name="smoking" />
+    </div>
+  );
 }
 
 function CheckboxVal(props) {
-    return (
-    <label forhtml="gudanggaramfilter">
-        <input type="checkbox" id={props.value} name={props.value} value={props.value} className="mx-3"/>
-        {props.text}
+  return (
+    <label htmlFor={props.value}>
+      <input
+        type="checkbox"
+        id={props.value}
+        value={props.value}
+        className="mx-3"
+        {...(props.register && props.name ? props.register(props.name) : {})}
+      />
+      {props.text}
     </label>
-    )
+  );
 }
 
-function Checkbox() {
-    return (
-    <div htmlFor="name" className="flex flex-col gap-5 py-5 px-10 border border-[#dadce0] rounded-md bg-[#fff] ">
-        <div className="title ">
-          <h1 className="text-[24px]">Jika anda pernah merokok, rokok apa yang pernah anda coba </h1>
-          <span className="text-[#1f1f1f]">Pilih beberapa jika anda pernah merokok </span>
-        </div>
-        <CheckboxVal value="gudanggaramfilter" text="Gudang garam filter" />
-        <CheckboxVal value="luckystrike" text="Lucky Strike" />
-        <CheckboxVal value="marlboro" text="Marlboro" />
-        <CheckboxVal value="esse" text="Esse" />
-      {/* <label forhtml="gudanggaramfilter">
-        <input type="checkbox" id="gudanggaramfilter" name="gudanggaramfilter" value="gudanggaramfilter" className="mx-3"/>
-        Gudang garam filter
-      </label> */}
+function Checkbox({ register }) {
+  return (
+    <div
+      htmlFor="name"
+      className="flex flex-col gap-5 py-5 px-10 border border-[#dadce0] rounded-md bg-[#fff] "
+    >
+      <div className="title ">
+        <h1 className="text-[24px]">
+          Jika anda pernah merokok, rokok apa yang pernah anda coba{" "}
+        </h1>
+        <span className="text-[#1f1f1f]">
+          Pilih beberapa jika anda pernah merokok{" "}
+        </span>
       </div>
-    )
+      <CheckboxVal value="gudanggaramfilter" text="Gudang garam filter" register={register} name="cigaretteBrands" />
+      <CheckboxVal value="luckystrike" text="Lucky Strike" register={register} name="cigaretteBrands" />
+      <CheckboxVal value="marlboro" text="Marlboro" register={register} name="cigaretteBrands" />
+      <CheckboxVal value="esse" text="Esse" register={register} name="cigaretteBrands" />
+    </div>
+  );
 }
 
 function Button(props) {
-    return <button className={`${props.styleColor} rounded w-[50%] py-1`}>{props.text}</button>
+  return (
+    <button type={props.type || "button"} onClick={props.onClick} className={`${props.styleColor} rounded w-[50%] py-1`}>
+      {props.text}
+    </button>
+  );
 }
 
-export { SingleInput, Button, RadioInputSmoke, RadioInputGender, Checkbox}
+export { SingleInput, Button, RadioInputSmoke, RadioInputGender, Checkbox };
